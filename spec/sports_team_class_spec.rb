0482df7f379aad05a7c6fcbc8b81fd5dc975f814
Ben Hamilton <ben.hamilton@me.com>
2019-04-08 15:45:26 +0100

    added a function to add a player, check if a player exists and updated points for a win

--- spec/sports_team_class_spec.rb
+++ spec/sports_team_class_spec.rb
@@ -25,7 +25,23 @@ def test_set_coach_name
   assert_equal('Jon',team.coach)
 end
 
+def test_add_new_player
+  team = SportsTeam.new('Codies',['Ben','Ru'],'Alex')
+  team.add_player('Alison')
+  assert_equal(['Ben','Ru','Alison'],team.players)
+end
 
+def test_does_player_exist?
+  team = SportsTeam.new('Codies',['Ben','Ru'],'Alex')
+  does_exist = team.does_player_exist('Ben')
+  assert(does_exist)
+end
+
+def test_if_team_won
+  team = SportsTeam.new('Codies',['Ben','Ru'],'Alex')
+  team.add_point_for_win('win')
+  assert_equal(1,team.points)
+end
 
 
 
--- sports_team_class.rb
+++ sports_team_class.rb
@@ -1,13 +1,27 @@
 class SportsTeam
 
-attr_accessor :team_name, :players, :coach
+  attr_accessor :team_name, :players, :coach, :points
 
-def initialize(team_name, players, coach)
-  @team_name = team_name
-  @players = players
-  @coach = coach
+  def initialize(team_name, players, coach)
+    @team_name = team_name
+    @players = players
+    @coach = coach
+    @points = 0
+  end
+
+  def add_player(name)
+    @players.push(name)
+  end
+
+  def does_player_exist(name)
+    return true if @players.include?(name)
+  end
+
+def add_point_for_win(result)
+  if result == 'win'
+    @points += 1
+  end
 end
-
 
 
 end
