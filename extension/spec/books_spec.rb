d810cca0824ee42611748afc2535d1935fd9a743
Ben Hamilton <ben.hamilton@me.com>
2019-04-08 16:53:37 +0100

    refactored the library array into a book list to improve readability

--- extension/spec/books_spec.rb
+++ extension/spec/books_spec.rb
@@ -4,65 +4,35 @@ require_relative('../books.rb')
 
 class TestLibrary < MiniTest::Test
 
+  def setup
+    @lotr = {
+      title: "lord_of_the_rings",
+      rental_details: {
+        student_name: "Jeff",
+        date: "01/12/16"
+      }
+    }
+    @harry_potter = {
+      title: "harry_potter",
+      rental_details: {
+        student_name: "Alan",
+        date: "05/04/16"
+      }
+    }
+    @book_list = [@lotr, @harry_potter]
+
+  end
+
 
   def test_get_title
-    book_list = Library.new(
-      [
-        {
-          title: "lord_of_the_rings",
-          rental_details: {
-            student_name: "Jeff",
-            date: "01/12/16"
-          }
-        },
-        {
-          title: "harry_potter",
-          rental_details: {
-            student_name: "Alan",
-            date: "05/04/16"
-          }
-        }
-      ]
-    )
-    book_list.get_books
-    assert_equal([
-      {
-        title: "lord_of_the_rings",
-        rental_details: {
-          student_name: "Jeff",
-          date: "01/12/16"
-        }
-      },
-      {
-        title: "harry_potter",
-        rental_details: {
-          student_name: "Alan",
-          date: "05/04/16"
-        }
-      }
-    ],book_list.get_books)
+    library = Library.new(@book_list)
+    library.get_books
+    assert_equal(@book_list, library.get_books)
   end
 
 def test_retrieve_book_from_title
-  book_list = Library.new(
-    [
-      {
-        title: "lord_of_the_rings",
-        rental_details: {
-          student_name: "Jeff",
-          date: "01/12/16"
-        }
-      },
-      {
-        title: "harry_potter",
-        rental_details: {
-          student_name: "Alan",
-          date: "05/04/16"
-        }
-      }
-    ]
-  )
-  book = book_list.book_info_from_title("harry_potter")
+  library = Library.new(@book_list)
+  book = library.book_info_from_title("harry_potter")
   assert_equal({
     title: "harry_potter",
     rental_details: {
@@ -73,30 +43,16 @@ def test_retrieve_book_from_title
 end
 
 def test_retrieve_rental_details_from_title
-  book_list = Library.new(
-    [
-      {
-        title: "lord_of_the_rings",
-        rental_details: {
-          student_name: "Jeff",
-          date: "01/12/16"
-        }
-      },
-      {
-        title: "harry_potter",
-        rental_details: {
-          student_name: "Alan",
-          date: "05/04/16"
-        }
-      }
-    ]
-  )
-  book = book_list.rental_info_from_title("harry_potter")
+  library = Library.new(@book_list)
+  book = library.rental_info_from_title("harry_potter")
   assert_equal({
       student_name: "Alan",
       date: "05/04/16"
     },
    book)
+   assert_equal("Alan", book[:student_name])
+   assert_equal("05/04/16", book[:date])
+
 end
 
 
