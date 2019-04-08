a98309ae59272ecb1469c9a4749d4607d3b63cc7
Ben Hamilton <ben.hamilton@me.com>
2019-04-08 15:29:36 +0100

    refactored to accomidate attr accessor

--- spec/sports_team_class_spec.rb
+++ spec/sports_team_class_spec.rb
@@ -6,23 +6,23 @@ class TestSportsTeam < MiniTest::Test
 
 def test_team_name
   team = SportsTeam.new('Codies',['Ben','Ru'],'Alex')
-  assert_equal('Codies', team.get_name)
+  assert_equal('Codies', team.team_name)
 end
 
 def test_player_names
   team = SportsTeam.new('Codies',['Ben','Ru'],'Alex')
-  assert_equal(['Ben','Ru'],team.get_players_names)
+  assert_equal(['Ben','Ru'],team.players)
 end
 
 def test_coach_name
   team = SportsTeam.new('Codies',['Ben','Ru'],'Alex')
-  assert_equal('Alex',team.get_coach_name)
+  assert_equal('Alex',team.coach)
 end
 
 def test_set_coach_name
   team = SportsTeam.new('Codies',['Ben','Ru'],'Alex')
-  team.set_coach_name('Jon')
-  assert_equal('Jon',team.get_coach_name)
+  team.coach=('Jon')
+  assert_equal('Jon',team.coach)
 end
 
 
